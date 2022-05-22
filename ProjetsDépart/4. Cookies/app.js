aa61debe0ad27856fb3ce94a887c1a03ee2d8f84
Bubs38 <burgain.aurelien@gmail.com>
2022-05-22 12:00:16 +0200

    Version 25

--- ProjetsDépart/4. Cookies/app.js
+++ ProjetsDépart/4. Cookies/app.js
@@ -1,3 +1,9 @@
+const affichage = document.querySelector(".affichage");
+const btns = document.querySelectorAll('button');
+const inputs = document.querySelectorAll('input');
+const infoTxt = document.querySelector('.info-txt');
+
+
 const today = new Date();
 const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 // console.log(nextWeek);
@@ -8,3 +14,25 @@ let year = today.getFullYear();
 // console.log(month);
 // console.log(year);
 document.querySelector("input[type=date]").value = `${year}-${month}-${day}`;
+
+btns.forEach(btn => {
+  btn.addEventListener('click', btnAction);
+})
+
+function btnAction(e) {
+
+  let nvObj = {};
+  inputs.forEach(input => {
+    let attrName = input.getAttribute('name');
+    let attrValeur = attrName !== "cookieExpire" ? input.value : input.valueAsDate;
+    nvObj[attrName] = attrValeur;
+  })
+  // console.log(nvObj);
+  let description = e.target.getAttribute('data-cookie');
+
+  if (description === "creer") {
+    creerCookie(nvObj.cookieName, nvObj.cookieValue, nvObj.cookieExpire);
+  } else if (description === 'toutAfficher'){
+    listeCookie();
+  }
+}
