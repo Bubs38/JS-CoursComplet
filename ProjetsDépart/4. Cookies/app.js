b8eea7b3f0db4f37580c23a0fdd162a13abdcb16
Bubs38 <burgain.aurelien@gmail.com>
2022-05-23 23:09:52 +0200

    Version 8

--- ProjetsDépart/4. Cookies/app.js
+++ ProjetsDépart/4. Cookies/app.js
@@ -2,6 +2,7 @@ const affichage = document.querySelector(".affichage");
 const btns = document.querySelectorAll('button');
 const inputs = document.querySelectorAll('input');
 const infoTxt = document.querySelector('.info-txt');
+let dejaFait = false;
 
 
 const today = new Date();
@@ -35,4 +36,74 @@ function btnAction(e) {
   } else if (description === 'toutAfficher'){
     listeCookie();
   }
+}
+
+function creerCookie(name, value, exp){
+
+  infoTxt.innerText = "";
+
+  // Si le cookie a un même nom
+  let cookies = document.cookie.split(";");
+  cookies.forEach( cookie => {
+
+    cookie = cookie.trim();
+    console.log(cookie);
+     let formatCookie = cookie.split('=');
+     console.log(formatCookie);
+     if(formatCookie[0] = encodeURIComponent(name)){
+       dejaFait = true;
+     }
+   })
+  
+  if(dejaFait) {
+    infoTxt.innerText = "Un cookie possède déjà ce nom.";
+    dejaFait = false;
+    return;
+  }
+
+
+  // Si le cookie n'a pas de nom
+  if(name.length === 0) {
+    infoTxt.innerText = "Impossible de définir un cookie sans nom";
+    return;
+  }
+
+  document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; expires=${exp.toUTCString()}`;
+  let info = document.createElement('li');
+  info.innerText = `Cookie ${name} créé.`;
+  affichage.appendChild(info);
+  setTimeout(() => {
+    info.remove()
+  }, 1500)
+}
+
+function listeCookie(){
+
+  let cookies = document.cookie.split(";");
+
+  if(cookies.join() === "") {
+    infoTxt.innerText = "Pas de cookies à afficher.";
+  }
+
+  cookies.forEach( cookie => {
+  
+    cookie = cookie.trim();
+    let formatCookie = cookie.split('=');
+    // console.log(formatCookie);
+
+    let item = document.createElement("li");
+
+    infoTxt.innerText = "Cliquez sur un cookie pour le supprimer";
+    item.innerText = `Nom : ${decodeURIComponent(formatCookie[0])}, Valeur : ${decodeURIComponent(formatCookie[1])}`;
+    affichage.appendChild(item);
+
+    // Supprimer un cookie
+    item.addEventListener('click', () => {
+      document.cookie = `${formatCookie[0]}=; expires=${new Date(0)}`
+      item.innerText = `Cookie ${formatCookie[0]} supprimé`;
+      setTimeout(() => {
+        item.remove();       
+      }, 1500)
+    });
+  })
 }
