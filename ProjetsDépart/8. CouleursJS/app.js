5ec7b447b6611c48190989a8f80ebf113b82e130
Bubs38 <burgain.aurelien@gmail.com>
2022-06-06 23:23:05 +0200

    Version 13

---
+++ ProjetsDépart/8. CouleursJS/app.js
@@ -0,0 +1,76 @@
+const inputsCouleur = document.querySelectorAll('.inp-couleur');
+const inputRange = document.querySelector('.inp-range');
+const btns = document.querySelectorAll('button');
+const fond = document.body;
+const containerCouleur = document.querySelector('.container-couleur');
+const span = document.querySelector('span');
+
+// Initialisation
+let valCouleur =['#BA5370', '#F4E2D8'];
+let inclinaison = 45;
+let index = 3;
+
+inputsCouleur[0].value  = valCouleur[0];
+inputsCouleur[1].value  = valCouleur[1];
+inputsCouleur[0].style.background = valCouleur[0];
+inputsCouleur[1].style.background = valCouleur[1];
+fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur[0]}, ${valCouleur[1]}`;
+
+// Inclinaison
+
+inputRange.addEventListener('input', (e) => {
+
+  inclinaison = e.target.value * 3.6;
+  fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur}`;
+
+})
+
+// Ajout et suppression 
+btns.forEach(btn => {
+  btn.addEventListener('click', rajouteEnleve);
+})
+
+function rajouteEnleve(e){
+
+  span.innerText = "";
+  const allInputs = document.querySelectorAll('.inp-couleur');
+  console.log(allInputs);
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  // console.log(randomColor);
+
+  if(e.target.className === "plus"){
+
+    if(allInputs.length > 8){
+      return;
+    }
+
+    const nvCouleur = document.createElement('input');
+    nvCouleur.setAttribute('class', '.inp-couleur');
+    nvCouleur.setAttribute('data-index', index);
+    nvCouleur.setAttribute('maxlength', 7);
+    nvCouleur.value = `#${randomColor.toUpperCase()}`;
+    nvCouleur.style.background = `#${randomColor}`;
+    containerCouleur.appendChild(nvCouleur);
+
+    valCouleur.push(`#${randomColor.toUpperCase()}`);
+
+    // MAJ fond
+    fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur})`;
+    index++;
+
+  }
+  else if(e.target.className === "moins"){
+
+    if(valCouleur.length === 2){
+      span.innerText = 'Il faut au moins deux couleurs';
+    } 
+    else {
+      valCouleur.pop();
+      allInputs[allInputs.length-1].remove();
+       // MAJ fond
+      fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur})`;
+
+    }
+  }
+
+}
